07503ca233e56a9d2e21990fb1663f3817d0055f
unknown <decaia@hotmail.com>
2018-03-22 20:54:51 -0300

    Clase 2 de Prog/Lab incluye videos de pasos para utilizar gitHub

---
+++ main.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define CANTIDAD_NUMEROS 3
+
+
+int main()
+{
+    int i;
+    int numeroUsuario;
+    int acumulador = 0;
+    float resultado;
+    int maximo;
+    int flag=0;
+    for(i=0 ; i < CANTIDAD_NUMEROS ; i++)
+    {
+        printf("Dame el Numero %d\n",i+1);
+        scanf("%d",&numeroUsuario);
+        acumulador = acumulador + numeroUsuario;
+
+        if((numeroUsuario>maximo)||flag==0){
+
+            maximo=numeroUsuario;
+            flag=1;
+        }
+    }
+    resultado = (float)acumulador / CANTIDAD_NUMEROS;
+    printf("Resultado %.2f\n",resultado);
+    printf("El numero maximo es %d\n", maximo);
+    return 0;
+}
